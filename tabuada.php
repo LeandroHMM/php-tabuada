205cbb00da860fc9ab3a0a30ecc81e96df3b137d
Leandro Moreira <leandro.manhani@gmail.com>
2021-07-22 20:13:40 -0300

    adicionados arquivos iniciais

---
+++ tabuada.php
@@ -0,0 +1,23 @@
+<?php
+    $num = $_GET['num'];
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabuada</title>
+</head>
+<body>
+    <?php for ($i = 1; $i <= 9; $i++): ?>
+        <h3> <?php echo 'Res: '. $i * $num; ?></h3>
+        <hr>
+    <?php endfor ?>
+            
+
+
+</body>
+</html>
